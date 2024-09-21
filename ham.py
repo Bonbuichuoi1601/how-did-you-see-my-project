6663b196ea7c770b579c892a732524bc7b6c3749
bruh12345678 <nhatt7164@gmail.com>
2024-09-21 14:26:27 +0700

    review bai code 2

--- ham.py
+++ ham.py
@@ -1,6 +1,9 @@
-def check_odd(bon : int) -> int:
+def check_odd(bon : int) -> int: #Hàm nhận int trả về bool
     return
+
 if __name__ == "__main__":
+
+    # Toàn bộ phía dưới chyển chuyển vào check_odd
     bon=int(input())
     if bon%2==1:
         print('le')
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ham import check_odd
 
-if __name__ == "__main__":
+if __name__ == "__main__": #chỉ chạy hàm check_odd trong main, lấy đầu ra để in kết quả
     bon=int(input())
     if bon%2==1:
         print('le')
