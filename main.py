11c178e7c0314b8d09e87233b4b208e5bc7e3648
bruh12345678 <nhatt7164@gmail.com>
2024-09-22 14:47:46 +0700

    Cập nhật bài tập về dạng list

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from ham import check_odd
 if __name__ == "__main__": #chỉ chạy hàm check_odd trong main, lấy đầu ra để in kết quả
+    n = int(input())
+    if check_odd(n):
+        print("chan")
+    else: 
+        print("le")
     bon=int(input())
     if check_odd(bon):
         print('le')
