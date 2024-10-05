80e8284f6c6d8d7535e1a9ed6c2ec68303cc9565
bonbuichuoi1601 <bonutung@gmail.com>
2024-10-05 16:20:32 +0700

    h

--- thebesr.py
+++ thebesr.py
@@ -1,5 +1,10 @@
-sophantu=int(input(''))
+sophantu=3
 a=[]
 for i in range (0,sophantu):
     a.append (int(input(f"Phan tu thu {i}:")))
-print (a)
+chuaam = 0
+enumeratecuaa=enumerate(a)
+for b in a:
+    chuaam=b
+    if chuaam==chuaam:
+        print(chuaam)
